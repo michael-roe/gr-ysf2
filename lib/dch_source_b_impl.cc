358c587380e0750556ed3f0255b1722314ec5b21
Michael Roe <mroe@cornstalk.org.uk>
2019-11-23 18:09:13 +0000

    dch_source_b: Ported to GNURadio 3.9.

---
+++ lib/dch_source_b_impl.cc
@@ -0,0 +1,107 @@
+/* -*- c++ -*- */
+/* 
+ * Copyright 2019 Michael Roe.
+ * 
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this software; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <gnuradio/io_signature.h>
+#include "dch_source_b_impl.h"
+
+namespace gr {
+  namespace ysf2 {
+
+    dch_source_b::sptr
+    dch_source_b::make(const std::string &callsign)
+    {
+      return gnuradio::get_initial_sptr
+        (new dch_source_b_impl(callsign));
+    }
+
+    /*
+     * The private constructor
+     */
+    dch_source_b_impl::dch_source_b_impl(const std::string &callsign)
+      : gr::sync_block("dch_source_b",
+              gr::io_signature::make(0, 0, sizeof(0)),
+              gr::io_signature::make(1, 1, sizeof(char)))
+    {
+      int i;
+      const char *call_str = callsign.c_str();
+      const char hwid[] = {0x46, 0x30, 0x64, 0x4f, 0x31};
+      /* This is the same hwid as the radio I used for interop testing */
+
+      set_output_multiple(10);
+      d_frame_number = 0;
+      memset(d_msg, ' ', 80);
+
+      /* Destination group - wildcard + source hardware id */
+      memset(d_msg, 42, 5);
+      memcpy(d_msg + 5, hwid, 5);
+
+      /* Source callsign */
+      strncpy(d_msg + 10, call_str, 10);
+      for (i = strlen(call_str); i<10; i++)
+      {
+        d_msg[10 + i] = ' ';
+      }
+
+      /* Transmitter id */
+      memcpy(d_msg + 55, hwid, 5);
+    
+      /* Location */
+      memset(d_msg + 60, 0, 20);
+    }
+
+    /*
+     * Our virtual destructor.
+     */
+    dch_source_b_impl::~dch_source_b_impl()
+    {
+    }
+
+    int
+    dch_source_b_impl::work(int noutput_items,
+        gr_vector_const_void_star &input_items,
+        gr_vector_void_star &output_items)
+    {
+      char *out = (char *) output_items[0];
+      int blocks;
+      int i;
+      int j;
+
+      blocks = noutput_items/10;
+      for (i=0; i<blocks; i++)
+      {
+        for (j=0; j<10; j++)
+        {
+          *out = d_msg[d_frame_number*10 + j];
+	  out++;
+	}
+	d_frame_number = (d_frame_number + 1) & 0x7;
+      }
+
+      // Tell runtime system how many output items we produced.
+      return noutput_items;
+    }
+
+  } /* namespace ysf2 */
+} /* namespace gr */
+
--- swig/ysf2_swig.i
+++ swig/ysf2_swig.i
@@ -12,6 +12,7 @@
 #include "ysf2/fich_parser_b.h"
 #include "ysf2/dch_errors_bb.h"
 #include "ysf2/dch_parser_b.h"
+#include "ysf2/dch_source_b.h"
 %}
 
 %include "ysf2/fich_source_b.h"
@@ -22,3 +23,5 @@ GR_SWIG_BLOCK_MAGIC2(ysf2, fich_parser_b);
 GR_SWIG_BLOCK_MAGIC2(ysf2, dch_errors_bb);
 %include "ysf2/dch_parser_b.h"
 GR_SWIG_BLOCK_MAGIC2(ysf2, dch_parser_b);
+%include "ysf2/dch_source_b.h"
+GR_SWIG_BLOCK_MAGIC2(ysf2, dch_source_b);
